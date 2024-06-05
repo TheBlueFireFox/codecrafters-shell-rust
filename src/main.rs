1cf28253de935fe3b5d788a2b74f1651336621b1
Adrian Hornung <adrian.hornung@yahoo.de>
2024-06-05 19:48:51 +0200

    pass The type builtin: builtins

--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,32 @@ enum Errors<'name> {
     IncorrectArgumentType(&'name str, &'name str),
 }
 
-fn builtins<'name>(com: &'name str, rest: &[&'name str]) -> Result<(), Errors<'name>> {
+enum Builtins {
+    Exit,
+    Echo,
+    Type,
+}
+
+impl<'input> TryFrom<&'input str> for Builtins {
+    type Error = Errors<'input>;
+
+    fn try_from(value: &'input str) -> Result<Self, Self::Error> {
+        match value {
+            "exit" => Ok(Self::Exit),
+            "echo" => Ok(Self::Echo),
+            "type" => Ok(Self::Type),
+            _ => Err(Errors::CommandNotFound(value)),
+        }
+    }
+}
+
+fn is_builtin(com: &str) -> Result<(), Errors<'_>> {
+    com.try_into().map(|_: Builtins| ())
+}
+
+fn run_builtins<'name>(com: Builtins, rest: &[&'name str]) -> Result<(), Errors<'name>> {
     match com {
-        "exit" => {
+        Builtins::Exit => {
             if rest.is_empty() {
                 return Err(Errors::MissingArgument("exit"));
             }
@@ -37,21 +60,47 @@ fn builtins<'name>(com: &'name str, rest: &[&'name str]) -> Result<(), Errors<'n
             }
             Err(Errors::IncorrectArgumentType(rest[0], "integer"))
         }
-        "echo" => {
+        Builtins::Echo => {
             println!("{}", rest.join(" "));
             io::stdout().flush().unwrap();
             Ok(())
         }
-        _ => Err(Errors::CommandNotFound(com)),
+        Builtins::Type => {
+            let com = rest[0];
+            if let Ok(v) = is_program(com) {
+                println!("{} is {}", com, v);
+            } else if is_builtin(com).is_ok() {
+                println!("{} is a shell builtin", com);
+            } else {
+                println!("{} not found", com)
+            }
+            io::stdout().flush().unwrap();
+            Ok(())
+        }
     }
+}
+
+fn is_program(com: &str) -> Result<String, Errors<'_>> {
+    Err(Errors::CommandNotFound(com))
+}
+
+fn run_program<'name>(com: &'name str, _rest: &[&'name str]) -> Result<(), Errors<'name>> {
+    Err(Errors::CommandNotFound(com))
 }
 
 fn run_commands(command: &str) -> Result<(), Errors> {
     let (com, rest) = command.split_once(' ').unwrap_or((command, ""));
     let parts: Vec<_> = rest.split_whitespace().collect();
     // TODO: match commands
-
-    builtins(com, &parts)
+    match run_program(com, &parts) {
+        Ok(_) => return Ok(()),
+        Err(Errors::CommandNotFound(_)) => {}
+        err @ Err(_) => return err,
+    }
+    match com.try_into() {
+        Ok(com) => run_builtins(com, &parts),
+        Err(_) => Err(Errors::CommandNotFound(com)),
+    }
 }
 
 fn repl() {
