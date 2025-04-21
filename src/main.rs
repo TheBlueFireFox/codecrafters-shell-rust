ede188fc1715e5a9495f4db894ed4ddb80b6ba1a
Adrian Hornung <adrian.hornung@yahoo.de>
2025-04-21 22:27:37 +0200

    pass Redirect stdout

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ mod args;
 
 use std::{
     borrow::Cow,
-    io::{self, Write},
+    fs::File,
+    io::{Stderr, Stdout, Write},
     path::PathBuf,
-    process::Command,
+    process::{Command, Stdio},
     str::FromStr,
 };
 
 use anyhow::Context as _;
+use itertools::Itertools;
 
 fn main() -> anyhow::Result<()> {
     repl()
@@ -30,6 +32,125 @@ enum Errors<'name> {
     IncorrectArgument(Cow<'name, str>),
     #[error("Io Error {0}")]
     IoError(#[from] std::io::Error),
+}
+
+enum RedirectIO<T: std::io::Write + Into<Stdio>> {
+    File(File),
+    Other(T),
+}
+
+impl<T> From<RedirectIO<T>> for Stdio
+where
+    T: std::io::Write + Into<Stdio>,
+{
+    fn from(value: RedirectIO<T>) -> Self {
+        match value {
+            RedirectIO::File(file) => file.into(),
+            RedirectIO::Other(other) => other.into(),
+        }
+    }
+}
+
+impl<T> std::io::Write for RedirectIO<T>
+where
+    T: std::io::Write + Into<Stdio>,
+{
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        match self {
+            RedirectIO::File(file) => file.write(buf),
+            RedirectIO::Other(other) => other.write(buf),
+        }
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        match self {
+            RedirectIO::File(file) => file.flush(),
+            RedirectIO::Other(other) => other.flush(),
+        }
+    }
+}
+
+struct Redirect {
+    stdout: RedirectIO<Stdout>,
+    stderr: RedirectIO<Stderr>,
+}
+
+impl Default for Redirect {
+    fn default() -> Self {
+        Self {
+            stdout: RedirectIO::Other(std::io::stdout()),
+            stderr: RedirectIO::Other(std::io::stderr()),
+        }
+    }
+}
+
+impl Redirect {
+    fn new(args: &[Cow<'_, str>]) -> std::io::Result<(Self, Option<usize>)> {
+        for (i, (operator, file_path)) in args.iter().tuple_windows().enumerate() {
+            match operator.as_ref() {
+                "1>" | ">" => {
+                    let stdout = File::options()
+                        .create(true)
+                        .truncate(true)
+                        .write(true)
+                        .open(file_path.as_ref())?;
+
+                    let s = Self {
+                        stdout: RedirectIO::File(stdout),
+                        ..Default::default()
+                    };
+
+                    return Ok((s, Some(i)));
+                }
+                "2>" => {
+                    let stderr = File::options()
+                        .create(true)
+                        .truncate(true)
+                        .write(true)
+                        .open(file_path.as_ref())?;
+
+                    let s = Self {
+                        stderr: RedirectIO::File(stderr),
+                        ..Default::default()
+                    };
+
+                    return Ok((s, Some(i)));
+                }
+                "1>>" => {
+                    let stdout = File::options()
+                        .create(true)
+                        .append(true)
+                        .open(file_path.as_ref())?;
+
+                    let s = Self {
+                        stdout: RedirectIO::File(stdout),
+                        ..Default::default()
+                    };
+
+                    return Ok((s, Some(i)));
+                }
+                "2>>" => {
+                    let stderr = File::options()
+                        .create(true)
+                        .append(true)
+                        .open(file_path.as_ref())?;
+
+                    let s = Self {
+                        stderr: RedirectIO::File(stderr),
+                        ..Default::default()
+                    };
+
+                    return Ok((s, Some(i)));
+                }
+                _ => {}
+            }
+        }
+
+        let stdout = RedirectIO::Other(std::io::stdout());
+        let stderr = RedirectIO::Other(std::io::stderr());
+
+        Ok((Self { stdout, stderr }, None))
+    }
 }
 
 enum Builtins {
@@ -69,6 +190,8 @@ impl State {
         &mut self,
         com: Builtins,
         rest: &[Cow<'name, str>],
+        stdout: &mut dyn std::io::Write,
+        _stderr: &mut dyn std::io::Write,
     ) -> Result<(), Errors<'name>> {
         match com {
             Builtins::Exit => {
@@ -87,21 +210,21 @@ impl State {
                 ));
             }
             Builtins::Echo => {
-                println!("{}", rest.join(" "));
+                writeln!(stdout, "{}", rest.join(" "))?;
             }
             Builtins::Type => {
                 let com = rest[0].clone();
                 if Self::is_builtin(com.as_ref()).is_ok() {
-                    println!("{} is a shell builtin", com);
+                    writeln!(stdout, "{} is a shell builtin", com)?;
                 } else if let Ok(v) = Self::is_program(&com) {
-                    println!("{} is {}", com, v);
+                    writeln!(stdout, "{} is {}", com, v)?;
                 } else {
                     println!("{} not found", com)
                 }
             }
             Builtins::Pwd => {
                 let p = format!("{:?}", self.path);
-                println!("{}", p.trim_matches('"'));
+                writeln!(stdout, "{}", p.trim_matches('"'))?;
             }
             Builtins::Cd => {
                 let mut old = self.path.clone();
@@ -125,7 +248,11 @@ impl State {
                     self.path = std::fs::canonicalize(new)?;
                 } else {
                     let p = format!("{:?}", new);
-                    println!("cd: {}: No such file or directory", p.trim_matches('"'));
+                    writeln!(
+                        stdout,
+                        "cd: {}: No such file or directory",
+                        p.trim_matches('"')
+                    )?;
                 }
             }
         }
@@ -153,6 +280,8 @@ impl State {
         &self,
         com: &Cow<'com, str>,
         rest: &[Cow<'com, str>],
+        stdout: impl Into<Stdio>,
+        stderr: impl Into<Stdio>,
     ) -> Result<(), Errors<'com>> {
         let _path = Self::is_program(com)?;
 
@@ -160,6 +289,8 @@ impl State {
         let args: Vec<_> = rest.iter().map(AsRef::as_ref).collect();
         let mut child = Command::new(com.as_ref())
             .args(args)
+            .stdout(stdout)
+            .stderr(stderr)
             .spawn()
             .expect("Failed to execute the child process");
 
@@ -174,14 +305,21 @@ impl State {
     }
 
     fn run_commands<'com>(&mut self, command: &'com str) -> Result<(), Errors<'com>> {
+        let args = args::process_args(command);
 
-        let args= args::process_args(command);
+        let (mut redirect, from) = Redirect::new(&args)?;
+
+        // make sure that the redirect information is not passed to processing
+        let args = match from {
+            Some(s) => &args[..s],
+            None => &args[..],
+        };
 
         if let Ok(com) = args[0].as_ref().try_into() {
-            return self.run_builtins(com, &args[1..]);
+            return self.run_builtins(com, &args[1..], &mut redirect.stdout, &mut redirect.stderr);
         }
 
-        match self.run_program(&args[0], &args[1..]) {
+        match self.run_program(&args[0], &args[1..], redirect.stdout, redirect.stderr) {
             Ok(_) => Ok(()),
             Err(Errors::CommandNotFound(_)) => Err(Errors::CommandNotFound(args[0].clone())),
             err @ Err(_) => err,
@@ -190,7 +328,10 @@ impl State {
 }
 
 fn repl() -> anyhow::Result<()> {
-    let stdin = io::stdin();
+    let stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
+    let mut stderr = std::io::stderr();
+
     let mut input = String::new();
 
     let mut state = State {
@@ -202,12 +343,14 @@ fn repl() -> anyhow::Result<()> {
         input.clear();
 
         // add promt
-        print!("$ ");
-        io::stdout().flush()?;
+        write!(&stdout, "$ ")?;
+
+        stdout.flush()?;
+        stderr.flush()?;
 
         let size = stdin.read_line(&mut input)?;
         if size == 0 {
-            println!();
+            writeln!(&stdout)?;
             break;
         }
         let input = input.trim();
@@ -217,22 +360,22 @@ fn repl() -> anyhow::Result<()> {
         match state.run_commands(input) {
             Ok(_) => state.last_exit_code = 0,
             Err(Errors::CommandNotFound(_)) => {
-                println!("{}: command not found", input);
+                writeln!(&stdout, "{}: command not found", input)?;
             }
             Err(Errors::ExitCode(v)) => {
                 state.last_exit_code = v;
             }
             Err(e @ Errors::MissingArgument(_)) => {
-                println!("{}", e);
+                writeln!(&stdout, "{}", e)?;
             }
             Err(e @ Errors::IncorrectArgumentType(_, _)) => {
-                println!("{}", e);
+                writeln!(&stdout, "{}", e)?;
             }
             Err(e @ Errors::IncorrectArgument(_)) => {
-                println!("{}", e);
+                writeln!(&stdout, "{}", e)?;
             }
             Err(e @ Errors::IoError(_)) => {
-                println!("{}", e);
+                writeln!(&stdout, "{}", e)?;
             }
         }
     }
